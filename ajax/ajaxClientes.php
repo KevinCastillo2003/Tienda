2aa19be369ad333e321b7bf08b261529cddaf0e8
KevinCastillo2003 <kc802683@gmail.com>
2023-07-06 00:53:26 -0500

    Primer Commit

---
+++ ajax/ajaxClientes.php
@@ -0,0 +1,26 @@
+<?php
+require_once "controlador/crtlClientes.php";
+require_once "../modelos/mdlClientes.php";
+class ajaxClientes{
+    public $idClientes;
+
+    public function cargarDatos(){
+        $tabla = "cliente";
+        $parametro= "cliente";
+        $id = $this->idClientes;
+        $datos = ControladorClientes::crtlCargarClientes($tabla, $parametro, $id);
+
+    }
+
+}
+if(isset($_POST{"idClientes"})){
+    $objAjaxCliente = new ajaxClientes();
+    $objAjaxCliente->idClientes = $_POST["idClientes"];
+    if($_POST["edit"] == "edit"){
+        $objAjaxClientes->cargarDatos();
+    }else{
+        
+        #$objAjaxCliente->eliminar();
+    }
+
+}
