91b01b3bdb081f68b600574880f2d5d882f213b5
KevinCastillo2003 <kc802683@gmail.com>
2023-07-07 11:18:25 -0500

    Nuevos Cambios

--- vistas/modulos/sidebar.php
+++ vistas/modulos/sidebar.php
@@ -14,7 +14,7 @@
           <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Steven Calva</a>
+          <a href="#" class="d-block">Kevin Castillo</a>
         </div>
       </div>
 
